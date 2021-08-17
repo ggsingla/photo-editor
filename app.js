5198df3de13adce39695f59c83d199c305a174b3
ggsingla <ggsingla777@gmail.com>
2021-08-17 23:28:43 +0530

    Sidebar elements

--- app.js
+++ app.js
@@ -1,73 +1,89 @@
 var options = [
-    {
-        name:"Brightness",
-        property:"brightness",
-        vaule: 100,
-        range:{
-            min:0,
-            max:200
-        },
-       unit:'%'
+  {
+    name: "Brightness",
+    property: "brightness",
+    vaule: 100,
+    range: {
+      min: 0,
+      max: 200,
     },
-    {
-        name:"Contrast",
-        property:"contrast",
-        vaule: 100,
-        range:{
-            min:0,
-            max:200
-        },
-       unit:'%'
+    unit: "%",
+  },
+  {
+    name: "Contrast",
+    property: "contrast",
+    vaule: 100,
+    range: {
+      min: 0,
+      max: 200,
     },
-    {
-        name:"Saturation",
-        property:"saturation",
-        vaule: 100,
-        range:{
-            min:0,
-            max:200
-        },
-       unit:'%'
+    unit: "%",
+  },
+  {
+    name: "Saturation",
+    property: "saturation",
+    vaule: 100,
+    range: {
+      min: 0,
+      max: 200,
     },
-    {
-        name:"Grayscale",
-        property:"grayscale",
-        vaule: 0,
-        range:{
-            min:0,
-            max:100
-        },
-       unit:'%'
+    unit: "%",
+  },
+  {
+    name: "Grayscale",
+    property: "grayscale",
+    vaule: 0,
+    range: {
+      min: 0,
+      max: 100,
     },
-    {
-        name:"Sepia",
-        property:"sepia",
-        vaule: 0,
-        range:{
-            min:0,
-            max:100
-        },
-       unit:'%'
+    unit: "%",
+  },
+  {
+    name: "Sepia",
+    property: "sepia",
+    vaule: 0,
+    range: {
+      min: 0,
+      max: 100,
     },
-    {
-        name:"Hue Rotate",
-        property:"hue-rotate",
-        vaule: 0,
-        range:{
-            min:0,
-            max:360
-        },
-       unit:'deg'
+    unit: "%",
+  },
+  {
+    name: "Hue Rotate",
+    property: "hue-rotate",
+    vaule: 0,
+    range: {
+      min: 0,
+      max: 360,
     },
-    {
-        name:"Blur",
-        property:"blur",
-        vaule: 0,
-        range:{
-            min:0,
-            max:30
-        },
-       unit:'px'
-    }
-]
-var image = document.querySelector(".image-view")
+    unit: "deg",
+  },
+  {
+    name: "Blur",
+    property: "blur",
+    vaule: 0,
+    range: {
+      min: 0,
+      max: 30,
+    },
+    unit: "px",
+  },
+];
+var image = document.querySelector(".image-view");
+var sidebar = document.querySelector(".sidebar");
+var rng = document.getElementById("inValue"); //input value
+
+function menuItem(name,x) {
+  var option = document.createElement("div");
+  option.classList.add("option");
+  option.setAttribute("id", x)
+  var prop = document.createElement("h3");
+  prop.innerHTML = name;
+  option.appendChild(prop);
+  sidebar.appendChild(option);
+}
+
+for (let x in options){
+    menuItem(options[x].name , x)
+}
